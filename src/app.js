2d1135e4156efcd8eb715c709bc5ef4f9dfa649b
CarlosJFer <carlossfernanezz@gmail.com>
2024-12-17 11:11:30 -0300

    Modificaciones

--- src/app.js
+++ src/app.js
@@ -27,20 +27,20 @@ app.use(cors({
 }));
 
 // Configurar el middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
 app.use('/api', mainRouter);
 
 app.get('/success', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'success', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'public', 'success', 'index.html'));
 });
 
 app.get('/failure', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'failure', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'public', 'failure', 'index.html'));
 });
 
 app.get('/pending', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'public', 'pending', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'public', 'pending', 'index.html'));
 });
 
 // Endpoint para crear una preferencia de pago
